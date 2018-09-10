2a77b750a703322e963702344e6541799f7b711a
Tirapat Numsinvichietchai <mind1990@gmail.com>
2018-09-10 02:30:00 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{ 	rank: "queen",
+	  	suit: "hearts",
+	  	cardImage: "images/queen-of-hears.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamons",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -12,8 +32,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }
 
